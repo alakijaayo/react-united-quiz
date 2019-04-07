936944d50179d00f05ad070c366bfdd415c86e5e
alakijaayo <alakijaayo@gmail.com>
2019-04-07 19:58:56 +0100

    Minor Adjustment
    Removed some options from the toolbar as I wanted the logo to be in the centre and it felt out of place. Going to use the logo as a click to get to the home page.

--- src/Components/Navigation/Toolbar/toolbar.js
+++ src/Components/Navigation/Toolbar/toolbar.js
@@ -4,13 +4,9 @@ import Logo from '../../Logo/logo';
 
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
-    <div>HOME</div>
     <div>Q.NUMBER</div>
     <Logo />
     <div>SCORE</div>
-    <nav>
-      ...
-    </nav>
   </header>
 )
 
