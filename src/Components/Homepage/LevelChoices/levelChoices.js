23cb1eead678a2c4ad080c94667c8a15cfd7ef1b
alakijaayo <alakijaayo@gmail.com>
2019-04-07 19:51:22 +0100

    Adding level buttons
    Added Level buttons to the homepage to allow a user choose between easy, medium and hard questions

--- src/Components/Homepage/LevelChoices/levelChoices.js
+++ src/Components/Homepage/LevelChoices/levelChoices.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import classes from './levelChoices.css'
+import classes from './levelChoices.css';
+import Button from '../../UI/Button/button';
 
 const levelChoices = (props) => {
   return (
     <div className={classes.LevelChoices}>
       <h2>Please choose the Level you wish to play:</h2>
+      <Button>Easy</Button>
+      <Button>Medium</Button>
+      <Button>Hard</Button>
     </div>
   )
 }
