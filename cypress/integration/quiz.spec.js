eae5a22a201a79573c4c5c8c6b123e8a1abd988e
alakijaayo <alakijaayo@gmail.com>
2019-04-07 20:08:50 +0100

    adding tests

--- cypress/integration/quiz.spec.js
+++ cypress/integration/quiz.spec.js
@@ -7,6 +7,26 @@ describe('Quiz', () => {
     cy.contains('Welcome to the Manchester United Quiz')
   })
 
+  it('gives the player the rules pt.1', () => {
+    cy.contains('A quiz for the true United fans.')
+  })
+
+  it('gives the player the rules pt.2', () => {
+    cy.contains('The rules of the game are simple,')
+  })
+
+  it('gives the player the rules pt.3', () => {
+    cy.contains('Choose your level and answer 25 questions')
+  })
+
+  it('gives the player the rules pt.4', () => {
+    cy.contains('About the greatest club in the world!')
+  })
+
+  it('gives the player the rules pt.5', () => {
+    cy.contains('Show your knowledge about the Red Devils!!')
+  })
+
   it('should ask the player to choose a level', () => {
     cy.contains('Please choose the Level you wish to play:')
   })
