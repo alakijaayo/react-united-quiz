ed6e674a0535c15adb77962148d4607146781f38
alakijaayo <alakijaayo@gmail.com>
2019-04-07 14:32:31 +0100

    Adding a Toolbar

--- src/Components/Layout/layout.js
+++ src/Components/Layout/layout.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Aux from '../../HOC/aux'
+import classes from './layout.css';
+import Toolbar from '../Navigation/Toolbar/toolbar';
 
 const layout = (props) => (
   <Aux>
-    <div>Toolbar</div>
-    <main>
+    <Toolbar />
+    <main className={classes.Content}>
       {props.children}
-    </main>  
+    </main>
   </Aux>
 )
 
---
+++ src/Components/Navigation/Toolbar/toolbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './toolbar.css';
+
+const toolbar = (props) => (
+  <header className={classes.Toolbar}>
+  <div>HOME</div>
+    <div>QUESTION NUMBER</div>
+    <div>LOGO</div>
+    <div>SCORE</div>
+  </header>
+)
+
+export default toolbar;
